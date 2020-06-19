54203341d49ceddeb1863acc6df457c5e4413c51
kangdari_ <ksh44820@naver.com>
2020-06-20 01:25:58 +0900

    add no scroll event

--- js/css.js
+++ js/css.js
@@ -1,6 +1,18 @@
-window.addEventListener("resize", () => {
-  const checkbox = document.querySelector("#gn_menuState");
-  if (matchMedia("screen and (min-width: 768px)").matches && checkbox.checked) {
-    checkbox.checked = false;
-  }
-});
+(() => {
+  // global_nav open하면 스크롤바 안생기도록 설정
+  const gn_label = document.querySelector(".gn_label");
+  gn_label.addEventListener("click", () => {
+    document.body.classList.toggle("gn_noScroll");
+  });
+
+  window.addEventListener("resize", () => {
+    const checkbox = document.querySelector("#gn_menuState");
+    if (
+      matchMedia("screen and (min-width: 768px)").matches &&
+      checkbox.checked
+    ) {
+      checkbox.checked = false;
+      document.body.classList.remove("gn_noScroll");
+    }
+  });
+})();
