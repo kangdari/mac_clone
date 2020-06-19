c728df3cd1a856092ee56cccd1ee1413e0ac6928
kangdari_ <ksh44820@naver.com>
2020-06-19 21:55:27 +0900

    create a responsive global_nav

--- js/css.js
+++ js/css.js
@@ -1,15 +1,6 @@
-(() => {
-  const gn_btn = document.querySelector(".gn_header .gn_menuIcon");
-  gn_btn.addEventListener("click", () => {
-    const gn_search_container = document.querySelector(".gn_search_container");
-    const gn_list = document.querySelector(".gn_list");
-    const gn = document.querySelector(".gn");
-    const gn_mark = document.querySelector(".gn_mark");
-
-    gn.classList.toggle("open_menu");
-    gn_search_container.classList.toggle("visible");
-    gn_list.classList.toggle("visible");
-    document.body.classList.toggle("open_menu");
-    gn_mark.classList.toggle("open_menu");
-  });
-})();
+window.addEventListener("resize", () => {
+  const checkbox = document.querySelector("#gn_menuState");
+  if (matchMedia("screen and (min-width: 768px)").matches && checkbox.checked) {
+    checkbox.checked = false;
+  }
+});
