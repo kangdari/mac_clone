a044e98beb0457afe701d81c72d9c2226249d3cc
kangdari_ <ksh44820@naver.com>
2020-06-21 19:17:22 +0900

    set scroll event && set current section 스크롤 이벤트가 발생할 때마다 scrollLoop 함수를 통해 현재 활성화 섹션 결정 global_nav, local_nav가 영역을 차지하지 않도록 position: absolute 설정

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 "use strict";
 (() => {
+  let yOffset = 0; // 현재 스크롤 위치, window.pageYoffset
+  let currentSection = 0; // 현재 활성화된 섹션
+  let prevScrollHeight = 0; // 현재 활성화 섹션 이전 섹션들의 높이 합
   // 각 section(scene)의 기본 정보
-  const sceneInfo = [
+  const sectionInfo = [
     // section_1
     {
       type: "sticky", // sticky :
@@ -42,14 +45,54 @@
 
   // 섹션 높이 설정
   const setLayout = () => {
-    for (let i = 0; i < sceneInfo.length; i++) {
-      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      sceneInfo[
+    for (let i = 0; i < sectionInfo.length; i++) {
+      sectionInfo[i].scrollHeight =
+        sectionInfo[i].heightNum * window.innerHeight;
+      sectionInfo[
         i
-      ].obj.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+      ].obj.container.style.height = `${sectionInfo[i].scrollHeight}px`;
     }
-    // console.log(sceneInfo);
+    // 현재 스크롤 위치
+    yOffset = window.pageYOffset;
+    // 현재 활성화 섹션 구하기
+    let totalHeight = 0;
+    for (let i = 0; i < sectionInfo.length; i++) {
+      totalHeight += sectionInfo[i].scrollHeight;
+      if (totalHeight > yOffset) {
+        currentSection = i; // 현재 활성화 섹션
+        break;
+      }
+    }
+    // 현재 활성화 섹션을 body의 id 값으로 설정
+    document.body.setAttribute("id", `show_section_${currentSection + 1}`);
   };
+
+  // 스크롤 이벤트 발생 시 현재 섹션 계산 후 반영
+  const scrollLoop = () => {
+    // 현재 섹션의 이전 섹션들의 높이 합
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentSection; i++) {
+      prevScrollHeight += sectionInfo[i].scrollHeight;
+    }
+    // 다음 섹션으로 바뀔 때
+    if (yOffset > prevScrollHeight + sectionInfo[currentSection].scrollHeight) {
+      currentSection++;
+      document.body.setAttribute("id", `show_section_${currentSection + 1}`);
+    }
+    // 이전 섹션으로 바뀔 때
+    if (prevScrollHeight > yOffset) {
+      // currentSection이 -1 되는 것을 방지
+      if (currentSection === 0) return;
+      currentSection--;
+      document.body.setAttribute("id", `show_section_${currentSection + 1}`);
+    }
+    console.log(currentSection + 1);
+  };
+
+  window.addEventListener("scroll", () => {
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  });
 
   window.addEventListener("resize", setLayout);
   window.addEventListener("load", setLayout);
